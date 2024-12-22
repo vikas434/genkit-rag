46a5cca00807774a9b9ac2bb8bb2b07556621665
vikas434 <98666287+vikas434@users.noreply.github.com>
2024-12-22 15:14:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Build gen AI features powered by your data with Firebase Genkit
+
+This is the code for [Build gen AI features powered by your data with Firebase Genkit](https://firebase.google.com/codelabs/ai-genkit-rag) codelab.
+
+## Getting Started
+
+First, update `src/lib/genkit/genkit.config.js` with your own Firebase project id and login using `gcloud auth application-default login`.
+See [Genkit documentation](https://firebase.google.com/docs/genkit/plugins/vertex-ai) for more information.
+
+Then install the dependencies:
+
+```bash
+npm install
+```
+
+Then run Genkit UI standalone sandbox:
+
+```bash
+npm run start:genkit
+```
+
+Alternatively, run Genkit UI alongside the Next.js development server:
+
+```bash
+npm run start
+```
+
+Open [http://localhost:4000](http://localhost:4000) with your browser to lauch Genkit UI.
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the Next.js app.
+
+You can start editing the app by modifying `genkit-functions/src/lib/itineraryFlow.ts`, `src/app/gemini/page.tsx`. The page auto-updates as you edit the file.
---
+++ src/lib/genkit/itineraryFlow.ts
@@ -0,0 +1,134 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  Place,
+  Destination,
+  ItineraryFlowInput as ItineraryFlowInput,
+  ItineraryFlowOutput as ItineraryFlowOutput,
+} from './types';
+import {
+  getActivitiesForDestination,
+  placesRetriever,
+} from './placesRetriever';
+import { ai } from './genkit.config';
+
+import { z } from 'zod';
+import { run } from 'genkit';
+
+export const ItineraryGeneratorPromptInput = ai.defineSchema(
+  'ItineraryGeneratorPromptInput',
+  z.object({
+    request: z.string(),
+    place: z.string(),
+    placeDescription: z.string(),
+    activities: z.array(
+      z.object({
+        name: z.string(),
+        description: z.string(),
+        imageUrl: z.string().optional(),
+      }),
+    ),
+  }),
+);
+
+const generateItinerary = async (request: string, place: Place) => {
+  const activities = await getActivitiesForDestination(place.ref);
+
+  const itineraryGenerator = await ai.prompt<
+    typeof ItineraryGeneratorPromptInput,
+    typeof Destination,
+    z.ZodTypeAny
+  >('itineraryGen');
+  const response = await itineraryGenerator({
+    request,
+    place: place.name,
+    placeDescription: place.knownFor,
+    activities,
+  });
+
+  const destination = response.data;
+  if (!destination) {
+    return null;
+  }
+  destination.itineraryImageUrl = place.imageUrl;
+  destination.placeRef = place.ref;
+  return destination;
+};
+
+export const itineraryFlow = ai.defineFlow(
+  {
+    name: 'itineraryFlow',
+    inputSchema: ItineraryFlowInput,
+    outputSchema: ItineraryFlowOutput,
+  },
+
+  async (tripDetails) => {
+    const imgDescription = '';
+    // TODO: 2. Replace the line above with this:
+    // const imgDescription = await run('imgDescription', async () => {
+    //   if (!tripDetails.imageUrls?.length) {
+    //     return '';
+    //   }
+    //   const imgDescription = await ai.prompt('imgDescription');
+    //   const result = await imgDescription({
+    //     input: { imageUrls: tripDetails.imageUrls },
+    //   });
+    //   return result.text;
+    // });
+
+    const places = await run(
+      'Retrieve matching places',
+      { imgDescription, request: tripDetails.request },
+      async () => {
+        const docs = await ai.retrieve({
+          retriever: placesRetriever,
+          query: `${tripDetails.request}\n${imgDescription}`,
+          options: {
+            limit: 3,
+          },
+        });
+        return docs.map((doc) => {
+          const data = doc.toJSON();
+          const place: Place = {
+            continent: '',
+            country: '',
+            imageUrl: '',
+            knownFor: '',
+            name: '',
+            ref: '',
+            tags: [],
+            ...data.metadata,
+          };
+          if (data.content[0].text) {
+            place.knownFor = data.content[0].text;
+          }
+          delete place.embedding;
+          return place;
+        });
+      },
+    );
+
+    const itineraries = await Promise.all(
+      places.map((place, i) =>
+        run(`Generate itinerary #${i + 1}`, () =>
+          generateItinerary(tripDetails.request, place),
+        ),
+      ),
+    );
+    return itineraries.filter((itinerary) => itinerary !== null);
+  },
+);
